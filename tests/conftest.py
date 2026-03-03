a964848c53fd3e443a4ddefc6f4df3dfc0871240
Jamison French <jamison@developmentseed.org>
2026-03-02 19:47:49 -0600

    chore: add mock region to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ import pytest
 def config_env_vars(monkeypatch):
     monkeypatch.setenv("FROM_EMAIL", "from@example.com")
     monkeypatch.setenv("EMAIL_BUCKET", "test-bucket")
+    monkeypatch.setenv("AWS_REGION", "us-west-2")
+    monkeypatch.setenv("AWS_DEFAULT_REGION", "us-west-2")
     monkeypatch.setenv(
         "FORWARD_MAPPING",
         '{"example@maap-project.org":["forward.address@maap-project.org"]}',
